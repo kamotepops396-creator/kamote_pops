1afef253db2be9e689ef63e055b242a86f593b67
Dev Armando <armandogranel2@gmail.com>
2026-04-21 14:44:02 +0800

    adding analytics by vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/next";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Analytics>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Analytics>
   </StrictMode>,
-)
+);
